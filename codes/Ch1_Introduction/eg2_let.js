009aab5037c4876483828f5c4a1d1ae55e9dd5a7
hegdeashwin <ashwin.hegde3@gmail.com>
2016-02-28 03:28:34 +0530

    Updates comments

--- codes/Ch1_Introduction/eg2_let.js
+++ codes/Ch1_Introduction/eg2_let.js
@@ -14,30 +14,34 @@ function message(name) {
   if(name) {
 
     /**
-     * Uncommenting below "var msg ... " will throw "msg is not defined" error.
+     * Uncommenting below "console.log(msg) or console.log(typeof msg)" will throw "ReferenceError: msg is not defined" error.
      */
     // console.log(msg);
+    // console.log(typeof msg);
 
     /**
      * Redeclaration of identifier is not allowed in the same scope.
-     * Uncommenting below "var msg ... " will throw "Identifier 'msg' has already been declared" error.
+     * Uncommenting below "var msg ... " will throw "SyntexError: Identifier 'msg' has already been declared" error.
      */
     // var msg = "Learning ES6/2015"; // Redeclaration
 
     let msg = "Hello World!";
 
     /**
-     * This will console "Hello World!".
+     * This will console "Hello World!" and "string".
      */
     console.log(msg);
+    console.log(typeof msg);
 
     return name + ", " + msg;
   } else {
 
     /**
-     * This will console "Learning ES6/2015".
+     * This will console "Learning ES6/2015" and "string".
      */
     console.log(msg);
+    console.log(typeof msg);
+
     return "Hi!";
   }
 }
