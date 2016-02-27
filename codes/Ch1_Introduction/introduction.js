79cd595d530e76e904244daa95f2593d5622a3a2
hegdeashwin <ashwin.hegde3@gmail.com>
2016-02-28 03:18:51 +0530

    Adds comments

--- codes/Ch1_Introduction/introduction.js
+++ codes/Ch1_Introduction/introduction.js
@@ -1,1 +1,10 @@
+/**
+ * Understanding `var` & `let` behavior in terms of declaration.
+ *
+ */
 require('./eg1_let');
+
+/**
+ * More understanding `var` & `let` behavior in terms of redeclaration.
+ */
+require('./eg2_let');
